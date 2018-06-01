3ada8138754b2814d1f4d95fc1650c5a5876b246
Edwin Chen <edwinchen85@gmail.com>
2018-06-01 22:07:46 +0800

    Cache-only strategy

--- public/sw.js
+++ public/sw.js
@@ -43,30 +43,37 @@ self.addEventListener('activate', function(event) {
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
+// self.addEventListener('fetch', function(event) {
   // console.log('[Service Worker] Fetching something ...', event);
   // event.respondWith(null);
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(function(response) {
+//         if (response) {
+//           return response;
+//         } else {
+//           return fetch(event.request)
+//             .then(function(res) {
+//               return caches.open(CACHE_DYNAMIC_NAME)
+//                 .then(function(cache) {
+//                   cache.put(event.request.url, res.clone());
+//                   return res;
+//                 })
+//             })
+//             .catch(function(err) {
+//               return caches.open(CACHE_STATIC_NAME)
+//                 .then(function(cache) {
+//                   return cache.match('offline.html')
+//                 });
+//             });
+//         }
+//       })
+//   );
+// });
+
+// Cache-only strategy
+self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        } else {
-          return fetch(event.request)
-            .then(function(res) {
-              return caches.open(CACHE_DYNAMIC_NAME)
-                .then(function(cache) {
-                  cache.put(event.request.url, res.clone());
-                  return res;
-                })
-            })
-            .catch(function(err) {
-              return caches.open(CACHE_STATIC_NAME)
-                .then(function(cache) {
-                  return cache.match('offline.html')
-                });
-            });
-        }
-      })
   );
 });
