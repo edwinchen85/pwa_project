8c8be6c60efcc105cc81316b600c887498177fcb
Edwin Chen <edwinchen85@gmail.com>
2018-06-01 22:27:49 +0800

    Network with cache fallback strategy

--- public/sw.js
+++ public/sw.js
@@ -79,8 +79,25 @@ self.addEventListener('activate', function(event) {
 // });
 
 // Network-only strategy
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     fetch(event.request)
+//   );
+// });
+
+// Network with cache fallback strategy
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     fetch(event.request)
+      .then(function(res) {
+        return caches.open(CACHE_DYNAMIC_NAME)
+          .then(function(cache) {
+            cache.put(event.request.url, res.clone());
+            return res;
+          })
+      })
+      .catch(function(err) {
+        return caches.match(event.request);
+      })
   );
 });
