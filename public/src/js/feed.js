b3f1d2242daef2d0bc9e35d4b3b5de5a3138cd2e
Edwin Chen <edwinchen85@gmail.com>
2018-06-02 20:02:30 +0800

    Register a synchronization task

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -132,4 +132,11 @@ form.addEventListener('submit', function(event) {
   }
 
   closeCreatePostModal();
+
+  if ('serviceWorker' in navigator && 'SyncManager' in window) {
+    navigator.serviceWorker.ready
+      .then(function(sw) {
+        sw.sync.register('sync-new-post');
+      });
+  }
 });
