b692b8ab9fa446adfa4bb0bc1d7a18f049c0cfc9
Edwin Chen <edwinchen85@gmail.com>
2018-06-05 18:18:42 +0800

    Open a page upon user interaction

--- public/sw.js
+++ public/sw.js
@@ -234,7 +234,22 @@ self.addEventListener('notificationclick', function(event) {
     notification.close();
   } else {
     console.log(action);
-    notification.close();
+    event.waitUntil(
+      clients.matchAll()
+        .then(function(clis) {
+          var client = clis.find(function(c) {
+            return c.visibilityState = 'visible';
+          });
+
+          if (client !== undefined) {
+            client.navigate('http://localhost:8000');
+            client.focus();
+          } else {
+            clients.openWindow('http://localhost:8000');
+          }
+          notification.close();
+        })
+    );
   }
 });
 
