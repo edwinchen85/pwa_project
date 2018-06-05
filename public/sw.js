df8e65319bf9ef7123000901ef7cfea8798b6d4b
Edwin Chen <edwinchen85@gmail.com>
2018-06-05 16:20:47 +0800

    React to notification close

--- public/sw.js
+++ public/sw.js
@@ -237,3 +237,7 @@ self.addEventListener('notificationclick', function(event) {
     notification.close();
   }
 });
+
+self.addEventListener('notificationclose', function(event) {
+  console.log('Notification was closed', event);
+});
